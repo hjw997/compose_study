089aa79d86bab63f1cbde03d99ca26c8c528d4be
whj <wanghaijun.425@163.com>
2024-06-26 08:11:04 +0800

    add:JetpackCompose book 重整目录结构。

---
+++ Compose_Book/JetpackComposeBook/chapter_03_bloom_ui_prictice/src/main/java/com/example/chapter_03_bloom_ui_prictice/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.chapter_03_bloom_ui_prictice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.chapter_03_bloom_ui_prictice.ui.theme.JetpackComposeBookTheme
+
+/**
+ * ReadMe ： ⚠️：直接学习 项目
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeBookTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeBookTheme {
+        Greeting("Android")
+    }
+}
